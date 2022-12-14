a0bed149abfbdb354386d7bc705f3dedaa797cda
Nathalya Tinoco <105314384+nathalyatinoco@users.noreply.github.com>
2022-12-14 13:22:40 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,123 @@
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($('#spinner').length > 0) {
+        $('#spinner').removeClass('show');
+      }
+    }, 1);
+  };
+  spinner();
+
+
+  // Initiate the wowjs
+  new WOW().init();
+  navBarToggle();
+
+  function navBarToggle() {
+    if ($(window).width() < 992) {
+      $('.fixed-top--sticky').removeClass('d-none').css('top', 0);
+      $('.fixed-top--default').addClass('d-none');
+    }
+    else {
+      if ($(document).scrollTop() > 100) {
+        $('.fixed-top--sticky').removeClass('d-none').css('top', 0);
+        $('.fixed-top--default').addClass('d-none');
+      }
+      else {
+        $('.fixed-top--sticky').addClass('d-none').css('top', 0);
+        $('.fixed-top--default').removeClass('d-none');
+      }
+    }
+  }
+
+  $(window).scroll(function () {
+    navBarToggle();
+  });
+
+  $(window).on('resize', function () {
+    navBarToggle();
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('.back-to-top').fadeIn('slow');
+    }
+    else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function () {
+    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000
+  });
+
+
+  // Project carousel
+  $(".project-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 25,
+    loop: true,
+    center: true,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>'
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      576: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      576: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+
+
+})(jQuery);
+
